f9f5a1ecf64ca381de3f68e0bd7bef9e47674702
shy2850 <shy2850@163.com>
2013-11-04 14:15:26 +0800

    POST请求由upload插件解析。
    原POST请求参数绑定在ajax下返回异常, 修改为使用 upload 插件进行解析。

--- nodeLib/common/handle.js
+++ nodeLib/common/handle.js
@@ -23,38 +23,29 @@ exports.execute = function(req,resp,root,str,mini,debug){
 		str = str.replace( temp[1] , vm );  			//替换关键字
 	}
 
-	var result = str, postData = "", run = function(t,f){f.call(this)};
-
-	req.addListener("data",function(d){
-		postData += d;
-	});
-
-	( (req.method==="GET" || req.forward) ? run : req.addListener )("end",function(d){
-		req.post = querystring.parse(postData);
-
-		try{
-			if(exports.conf.runJs){
-				var x = "", strs = str.split(/<%|%>/);
-				for (var i = 0; i < strs.length; i++) {		//偶数为HTML片段，奇数为js代码片段，分别处理转义符和换行
-					x += (i%2 === 0) ? strs[i].replace(/\\/g,"\\\\").replace(/[']/g,"\\'") : "<%"+strs[i].replace(/[\n\r]/g," ").replace(/echo/g,'_output_+=')+"%>";
-				};
-				str = x.replace(/<%=/g,"'; _output_ += ").replace(/<%/g,"';").replace(/%>/g,"; _output_+='").replace(/[\n\r]/g,"\\n");
-				//以上语句通过替换关键标识符，获得js执行片段。
-				
-				//拼装function并且执行，以获取结果。如果结果不是function，当作string输出，否则，执行function，参见demo中的login.htm。
-				result = new Function("request","response", "var _o_ = null, _output_='" + str + "'; return _o_ || _output_;")(req,resp);
-			}
-
-			switch(typeof result){
-				case "function": result(); return;
-				case "string":  
-				default : debug ? resp.end( result ) : mini(result,resp);
-			}
-		}catch(e){
-			resp.writeHead(500, {"Content-Type": "text/html"});
-			resp.end( e.stack.toString().replace(/\n/g,"<br>") );
+	var result = str;
+	try{
+		if(exports.conf.runJs){
+			var x = "", strs = str.split(/<%|%>/);
+			for (var i = 0; i < strs.length; i++) {		//偶数为HTML片段，奇数为js代码片段，分别处理转义符和换行
+				x += (i%2 === 0) ? strs[i].replace(/\\/g,"\\\\").replace(/[']/g,"\\'") : "<%"+strs[i].replace(/[\n\r]/g," ").replace(/echo/g,'_output_+=')+"%>";
+			};
+			str = x.replace(/<%=/g,"'; _output_ += ").replace(/<%/g,"';").replace(/%>/g,"; _output_+='").replace(/[\n\r]/g,"\\n");
+			//以上语句通过替换关键标识符，获得js执行片段。
+			
+			//拼装function并且执行，以获取结果。如果结果不是function，当作string输出，否则，执行function，参见demo中的login.htm。
+			result = new Function("request","response", "var _o_ = null, _output_='" + str + "'; return _o_ || _output_;")(req,resp);
 		}
-	});
+
+		switch(typeof result){
+			case "function": result(); return;
+			case "string":  
+			default : debug ? resp.end( result ) : mini(result,resp);
+		}
+	}catch(e){
+		resp.writeHead(500, {"Content-Type": "text/html"});
+		resp.end( e.stack.toString().replace(/\n/g,"<br>") );
+	}
 
 		
 	
