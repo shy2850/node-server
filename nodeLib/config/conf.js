972324c7b262ac11b3358b59314d6a27208e768d
shy2850 <shy2850@163.com>
2014-07-22 13:31:28 +0800

    更新配置文件和npm保持一致

--- nodeLib/config/conf.js
+++ nodeLib/config/conf.js
@@ -3,7 +3,7 @@
  **/
 exports.CONF = {
     //root:"C:\\Users\\Administrator\\Desktop\\webHarvest\\cardistry2.0\\cardistry",
-    root :"d:\\doc\\",
+    root :"D:\\WORK\\XhcmsProject\\Tomcat\\webapps\\xhcms\\xhcms_2014\\",
 //    root: "D:\\WORK\\web\\webDevelop\\space\\trunk",       //服务器索引的根目录，可配置为任意本地地址
     welcome: "",    //使用欢迎页面的文件名，为空时，表示不使用欢迎页面
     notFound: __dirname + "/../html/404.html",      //访问的资源不存在是，跳转的页面配置
@@ -37,3 +37,36 @@ exports.staticConf = {          //不要修改
     expires : 1000*60*60*24     //服务端缓存时间设置
 };
 
+exports.conf1 = {          //不要修改
+    root: "C:\\Users\\SHY2850\\Desktop\\temp\\",       
+    welcome: "",
+    notFound: __dirname + "/../html/404.html",
+    folder: __dirname + "/../html/folder.html",
+    handle: true,
+    coffee: true,
+    less: true,
+    debug: false,
+    fs_mod: true,
+    port: 1000,
+    maxConnections: 1000,    //并发处理的最大连接数
+    runJs : true,
+    output: "c:\\output\\",
+    expires : 1000*60*60*24     //服务端缓存时间设置
+};
+
+exports.conf2 = {          //不要修改
+    root: "D:\\WORK\\web\\UIdesign\\集成交互项目组\\",       
+    welcome: "",
+    notFound: __dirname + "/../html/404.html",
+    folder: __dirname + "/../html/folder.html",
+    handle: true,
+    coffee: true,
+    less: true,
+    debug: false,
+    fs_mod: true,
+    port: 404,
+    maxConnections: 1000,    //并发处理的最大连接数
+    runJs : true,
+    output: "c:\\output\\",
+    expires : 1000*60*60*24     //服务端缓存时间设置
+};
