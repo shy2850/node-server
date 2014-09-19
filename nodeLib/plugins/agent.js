a44dccf51bc6ba482730a76ab38b478df7eaa591
shy2850 <shy2850@163.com>
2014-09-19 19:53:30 +0800

    prettify插件修改。

--- nodeLib/plugins/agent.js
+++ nodeLib/plugins/agent.js
@@ -4,14 +4,7 @@ exports.execute = function(req,resp,root){
 	var query = decodeURI( url.parse( req.url ).query).replace(/.*(http[s]?[:])/,"$1"), info="";
 	http.get(query, function(res) {
 		res.setEncoding('utf-8');
-		res.on('data',function(data){
-           info += data;
-		});
-        res.on('end',function(data){
-            resp.writeHead(200, {"Content-Type": res.headers["content-type"]});
-            resp.end(info);
-        });
-
+		res.pipe(resp);
 	}).on('error', function(e) {
         resp.writeHead(500, {"Content-Type": "text/html"});
 	  	resp.end( e.stack.toString().replace(/\n/g,"<br>") );
--- nodeLib/plugins/prettify.js
+++ nodeLib/plugins/prettify.js
@@ -2,9 +2,13 @@ var url = require('url'),
     fs = require('fs'),
     prettifyModel = __dirname + "/../html/prettify.html";
 exports.execute = function(req,resp,root,handle,f,conf){
-	var $ = req.$;
-	$.title = decodeURI( url.parse( req.url ).query).replace(/.*(http[s]?[:])/,"$1");
-	fs.readFile(prettifyModel,function (err,data){ 
+	var $ = req.$, 
+		_url = url.parse( req.url ),
+		query = decodeURI( _url.query);
+
+	query.replace(/.*(http[:])?/,"$1");
+	$.title = query;
+	fs.readFile(prettifyModel,function (err,data){
 		if(err)console.log(err);
 	    handle.execute(req,resp,root,data.toString(),f,true,conf);
 	});
