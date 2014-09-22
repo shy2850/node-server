15cb95ca12069875e5c32cd04329fe6a7739375b
时扬扬 <shiyangyang@news.cn>
2014-09-22 09:55:19 +0800

    agent插件BUG修复

--- nodeLib/plugins/agent.js
+++ nodeLib/plugins/agent.js
@@ -1,12 +1,22 @@
 var http = require('http'),
-	url = require('url');
-exports.execute = function(req,resp,root){
-	var query = decodeURI( url.parse( req.url ).query).replace(/.*(http[s]?[:])/,"$1"), info="";
-	http.get(query, function(res) {
-		res.setEncoding('utf-8');
-		res.pipe(resp);
-	}).on('error', function(e) {
-        resp.writeHead(500, {"Content-Type": "text/html"});
-	  	resp.end( e.stack.toString().replace(/\n/g,"<br>") );
-	});
+	url = require('url'),
+	fs = require('fs');
+exports.execute = function(req,resp,root,handle,f,conf){
+	var query = decodeURI( url.parse( req.url ).query);
+		_url = /.*(http[s]?[:])/.test(query)? query.replace(/.*(http[s]?[:])/,"$1"): null;
+	if(_url){
+		http.get(_url, function(res) {
+			res.setEncoding('utf-8');
+			res.pipe(resp);
+		}).on('error', function(e) {
+	        resp.writeHead(500, {"Content-Type": "text/html"});
+		  	resp.end( e.stack.toString().replace(/\n/g,"<br>") );
+		});
+	}else{
+		fs.readFile(root+query.split('?')[0],function (err,data){
+	        if(err)console.log(err);
+	        handle.execute(req,resp,root,data.toString(),f,true,conf);
+	    });
+	}
+		
 }
