d3f14d4e12438c28ee6ef3ef6fe8cb4bc91e663c
shy2850 <shy2850@163.com>
2014-10-07 19:34:50 +0800

    #prettify插件修改成服务端的highlight实现, 去除客户端支持方案, [不设置发布环境依赖, 需要手工安装highlight~0.2.3]

--- nodeLib/plugins/agent.js
+++ nodeLib/plugins/agent.js
@@ -2,21 +2,16 @@ var http = require('http'),
 	url = require('url'),
 	fs = require('fs');
 exports.execute = function(req,resp,root,handle,f,conf){
-	var query = decodeURI( url.parse( req.url ).query);
-		_url = /.*(http[s]?[:])/.test(query)? query.replace(/.*(http[s]?[:])/,"$1"): null;
-	if(_url){
-		http.get(_url, function(res) {
-			res.setEncoding('utf-8');
-			res.pipe(resp);
-		}).on('error', function(e) {
-	        resp.writeHead(500, {"Content-Type": "text/html"});
-		  	resp.end( e.stack.toString().replace(/\n/g,"<br>") );
-		});
-	}else{
-		fs.readFile(root+query.split('?')[0],function (err,data){
-	        if(err)console.log(err);
-	        new handle.execute(req,resp,root,data.toString(),f,true,conf);
-	    });
-	}
+	var query = decodeURI( url.parse( req.url ).query),
+		_url = /.*(http[s]?[:])/.test(query)? query.replace(/.*(http[s]?[:])/,"$1") : 'http://'+req.headers.host+query;
+	
+	http.get(_url, function(res) {
+		resp.writeHead(res.statusCode, res.headers);
+		res.setEncoding('utf-8');
+		res.pipe(resp);
+	}).on('error', function(e) {
+        resp.writeHead(500, {"Content-Type": "text/html"});
+	  	resp.end( e.stack.toString().replace(/\n/g,"<br>") );
+	});
 		
 }
--- nodeLib/plugins/prettify.js
+++ nodeLib/plugins/prettify.js
@@ -1,15 +1,25 @@
-var url = require('url'),
+var http = require('http'),
+	url = require('url'),
     fs = require('fs'),
-    prettifyModel = __dirname + "/../html/prettify.html";
-exports.execute = function(req,resp,root,handle,f,conf){
-	var $ = req.$, 
-		_url = url.parse( req.url ),
-		query = decodeURI( _url.query);
+    highlight = require('highlight').Highlight;
+var _style = 'default',	 //可选项:ascetic,brown_paper,dark,default,far,github,idea,ir_black,magula,school_book,sunburst,vs,zenburn
+	style = fs.readFileSync( "node_modules/highlight/lib/vendor/highlight.js/styles/"+_style+".css",'utf-8');
 
-	query.replace(/.*(http[:])?/,"$1");
-	$.title = query;
-	fs.readFile(prettifyModel,function (err,data){
-		if(err)console.log(err);
-	    new handle.execute(req,resp,root,data.toString(),f,true,conf);
+exports.execute = function(req,resp,root,handle,f,conf){
+	var query = decodeURI( url.parse( req.url ).query), data = '',
+		_url = /.*(http[s]?[:])/.test(query)? query.replace(/.*(http[s]?[:])/,"$1") : 'http://'+req.headers.host+query;
+
+	http.get(_url, function(res) {
+		resp.writeHead(res.statusCode, {"Content-Type": "text/html"});
+		res.setEncoding('utf-8');
+		res.on('data',function(d){
+			data += d
+		}).on('end',function(){
+			resp.end( '<!DOCTYPE html><html><head><meta charset="UTF-8" /><title>'+_url.split('/').pop()+'</title><style>'+style+'</style></head><body><pre>'+highlight(data)+'</pre></body></html>');
+		});
+	}).on('error', function(e) {
+        resp.writeHead(500, {"Content-Type": "text/html"});
+	  	resp.end( e.stack.toString().replace(/\n/g,"<br>") );
 	});
+		
 }
