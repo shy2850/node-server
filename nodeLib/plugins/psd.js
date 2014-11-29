a50b58ec06449758d1e0cfd483758e585d211d6a
shy2850 <shy2850@163.com>
2014-11-30 00:14:27 +0800

    #修改psd插件, 开发PSD图片元素拆分功能, 将其中的元素保存, 输出layers基本信息[psd.js 内存消耗极大, 成功率有限]

--- nodeLib/plugins/psd.js
+++ nodeLib/plugins/psd.js
@@ -2,25 +2,47 @@
 var url = require('url'),
     mime = require('mime'),
     fs = require('fs');
-exports.execute = function(req, resp, root){
-    var path = root + decodeURI( url.parse( req.url ).query),
-        pngPath = path.replace(/^(.*?)\.psd$/i,'$1' + '.png');
+
+function saveAllLayers( psd, path, resp){
     try{
-        require('psd').open(path).then(function(psd){
-            return psd.image.saveAsPng( pngPath );
-        }).then(function(){
-            fs.readFile(pngPath, function(err, data){
-                if(err){
-                    console.log( err );
-                }
-                resp.writeHead(200, {
-                    "Content-Type": mime.get('png')
-                });
-                resp.end(data);
+        fs.mkdir( path.replace(/([^\\\/]*?)\.png$/i,'$1') ,function(){
+            psd.layers.forEach(function(t, i){
+                t.image.saveAsPng( path.replace(/([^\\\/]*?)\.png$/i,'$1/' + i + '.' + t.legacyName.replace(/\W+/g,'_') + '.png') );
             });
         });
+        var str = JSON.stringify( psd.tree().export(), undefined, 2 ); //JSON.stringify(psd.layers, undefined, 4);
+        fs.writeFile( path.replace(/([^\\\/]*?)\.png$/i,'$1/layers.json'), str);
     }catch(e){
         resp.writeHead(500, {"Content-Type": "text/html"});
         resp.end( e.stack.toString().replace(/\n/g,"<br>") );
     }
+}
+
+function getPng(path, pngPath, resp){
+    var psd;
+    fs.readFile(pngPath, function(err, data){
+        if(err){
+            require('psd').open(path).then(function(p){
+                psd = p;
+                return p.image.saveAsPng( pngPath );
+            }).then(function(){
+                setTimeout(function(){
+                    saveAllLayers( psd, pngPath, resp);
+                },0);
+                getPng(path, pngPath, resp);
+            });
+        }else{
+            resp.writeHead(200, {
+                "Content-Type": mime.get('png')
+            });
+            resp.end(data);
+        }
+    });
+}
+
+exports.execute = function(req, resp, root){
+    var path = root + decodeURI( url.parse( req.url ).query),
+        pngPath = path.replace(/([^\\\/]*?)\.psd$/i,'$1.png');
+
+    getPng(path, pngPath, resp);
 };
