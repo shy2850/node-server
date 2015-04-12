bd4ed985022d26d096dec302c2bf0dbc86b40d90
shy2850 <shy2850@163.com>
2015-04-12 09:53:15 +0800

    1.5.2修正

--- node-server.js
+++ node-server.js
@@ -6,8 +6,8 @@ var ports = {}, extCmd = ([]).slice.call( process.argv, 2 ).join(' ');
 for(var k in CONF){
     if(!ports[CONF[k].port]){
         ports[CONF[k].port] = server.start(CONF[k]);
-        console.log("Server running at http://127.0.0.1:" + CONF[k].port + '\t[' + k + ']');
     }
+    console.log("Server running at http://127.0.0.1:" + CONF[k].port + '\t[' + k + ']');
 }
 
 if(extCmd){ require('child_process').exec( extCmd ); }
