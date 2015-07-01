d8d4991749e54aa784c154394f1508e84b9c8af5
shiyangyang <shiyangyang@news.cn>
2015-07-01 11:27:09 +0800

    cdn 的mime识别需要除去search参数

--- nodeLib/filter/cdn.js
+++ nodeLib/filter/cdn.js
@@ -17,7 +17,7 @@ exports.execute = function( req, resp, stats ) {
         }else{
             resp.writeHead(200, {
                 "Content-Encoding": resp.gzip ? "gzip" : "utf-8",
-                "Content-Type": req.util.mime.get(k)
+                "Content-Type": req.util.mime.get(k.split("?")[0])
             });
             resp.end( source.data );
         }
