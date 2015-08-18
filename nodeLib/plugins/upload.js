fd6143cdd44795e7381d46bf11af5874d55ab3b5
shy2850 <shy2850@163.com>
2015-08-18 09:29:37 +0800

    upload 不存在目标模板时，抛出异常，防止服务宕机

--- nodeLib/plugins/upload.js
+++ nodeLib/plugins/upload.js
@@ -36,11 +36,13 @@ exports.execute = function(req, resp, root, handle, conf, modelPath){
                 var extType = path.extname(uploadModel).substring(1);
                 fs.readFile(uploadModel, function(err, data){
                     if(err){
-                        throw err;
+                        resp.writeHead(500, {"Content-Type": "text/html"});
+                        resp.end( err.toString().replace(/\n/g,"<br>") );
+                    }else{
+                        resp.writeHead(200, {'content-type': mime.get(extType)});
+                        req.forward = true;
+                        handle.execute.call(req,req,resp,root,data.toString(),mini,true,conf);
                     }
-                    resp.writeHead(200, {'content-type': mime.get(extType)});
-                    req.forward = true;
-                    handle.execute.call(req,req,resp,root,data.toString(),mini,true,conf);
                 });
             });
         form.parse(req);
